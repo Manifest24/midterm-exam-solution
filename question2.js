ffa85bb75cfb5c91d244f50e08d9eb92057facc0
Manifest24 <120199431+Manifest24@users.noreply.github.com>
2025-03-11 20:26:23 +0800

    Update question2.js

--- question2.js
+++ question2.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+app.get('/test', (req, res) => {
+    res.json({ message: 'Express is working! Tristan Jesus V. Elvinia' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
