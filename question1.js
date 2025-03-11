4135bfe164a6e75ce6b098f192bd233624473a3e
Manifest24 <120199431+Manifest24@users.noreply.github.com>
2025-03-11 20:36:16 +0800

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,7 +1,8 @@
 function countProperties(obj) {
-    return Object.keys(obj).length;
+    const propertyNames = Object.keys(obj); // Get an array of property names
+    return propertyNames.length; // Return the number of properties
 }
 
 // Example usage:
 const exampleObject = { a: 1, b: 2, c: 3 };
-console.log(countProperties(exampleObject)); 
+console.log(countProperties(exampleObject)); // Output: 3
